8b4d98ac5582f1a075f593c961e93f7775cc1a2d
Kargisa <personal@kargisa.com>
2024-03-02 00:34:13 +0100

    flowbite + yarn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,30 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { Alert } from 'flowbite-svelte';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<Alert>
+	<span class="font-medium">Default alert!</span>
+	Change a few things up and try submitting again.
+</Alert>
+<Alert color="blue">
+	<span class="font-medium">Info alert!</span>
+	Change a few things up and try submitting again.
+</Alert>
+<Alert color="red">
+	<span class="font-medium">Danger alert!</span>
+	Change a few things up and try submitting again.
+</Alert>
+<Alert color="green">
+	<span class="font-medium">Success alert!</span>
+	Change a few things up and try submitting again.
+</Alert>
+<Alert color="yellow">
+	<span class="font-medium">Warning alert!</span>
+	Change a few things up and try submitting again.
+</Alert>
+<Alert color="dark">
+	<span class="font-medium">Dark alert!</span>
+	Change a few things up and try submitting again.
+</Alert>
+  
+  
