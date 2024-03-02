6cbcbffd500c292049a106d2e09e20abbd9de744
kargisa <luca.wolf@outlook.com>
2024-03-03 00:40:24 +0100

    first appshell

---
+++ src/components/about/cv.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim commodi non alias aperiam veritatis
+voluptate ipsum placeat ex quis, earum eum adipisci, optio minima eos sit, voluptatem neque magni
+sunt!
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,68 @@
-<script>
-	import { Alert } from 'flowbite-svelte';
+<script lang="ts">
+	import { ChevronRight, ChevronDown, ScrollText } from 'lucide-svelte';
+	import Cv from '../components/about/cv.svelte';
+
+	let extendAboutMe: boolean = false;
+	let extendProjects: boolean = false;
 </script>
 
-<Alert>
-	<span class="font-medium">Default alert!</span>
-	Change a few things up and try submitting again.
-</Alert>
-<Alert color="blue">
-	<span class="font-medium">Info alert!</span>
-	Change a few things up and try submitting again.
-</Alert>
-<Alert color="red">
-	<span class="font-medium">Danger alert!</span>
-	Change a few things up and try submitting again.
-</Alert>
-<Alert color="green">
-	<span class="font-medium">Success alert!</span>
-	Change a few things up and try submitting again.
-</Alert>
-<Alert color="yellow">
-	<span class="font-medium">Warning alert!</span>
-	Change a few things up and try submitting again.
-</Alert>
-<Alert color="dark">
-	<span class="font-medium">Dark alert!</span>
-	Change a few things up and try submitting again.
-</Alert>
-  
-  
+<div class="h-screen w-screen flex-col overflow-hidden">
+	<div class="flex h-5 w-full border-b-2">
+		<!-- PUT TOOLBAR HERE -->
+	</div>
+	<div class="flex h-full">
+		<div class="h-full w-64 space-y-2 border-r-2 p-2">
+			<!-- PUT FILE EXPLORER HERE -->
+			<div>EXPLORER</div>
+
+			<div>
+				<button
+					class="flex items-center"
+					on:click={() => {
+						extendAboutMe = !extendAboutMe;
+					}}
+				>
+					{#if extendAboutMe}
+						<ChevronDown class="size-5" />
+					{:else}
+						<ChevronRight class="size-5" />
+					{/if}
+					About Me
+				</button>
+				{#if extendAboutMe}
+					<div class=" pl-6">
+						<button class="flex w-full items-center space-x-3">
+							<ScrollText class="size-5" />
+							<p>cv.pdf</p>
+						</button>
+					</div>
+				{/if}
+			</div>
+			<div>
+				<button
+					class="flex items-center"
+					on:click={() => {
+						extendProjects = !extendProjects;
+					}}
+				>
+					{#if extendProjects}
+						<ChevronDown class="size-5" />
+					{:else}
+						<ChevronRight class="size-5" />
+					{/if}
+					Projects
+				</button>
+			</div>
+		</div>
+		<div class="flex w-full flex-col justify-center p-2">
+			<div class="flex flex-col items-center">
+				<h1 class="text-4xl font-bold">Luca Wolf</h1>
+				<p>Kargisa</p>
+			</div>
+			<div class="h-full">
+				<!-- PUT FILE CONTENT HERE -->
+				<Cv />
+			</div>
+		</div>
+	</div>
+</div>
