e66717251ee5be660a03df01997bafbe7a48b2ca
Kargisa <luca.wolf@outlook.com>
2024-03-04 23:26:01 +0100

    md import

---
+++ src/posts/aboutme.md
@@ -0,0 +1,9 @@
+---
+title: Second
+description: Second post.
+date: '2023-4-16'
+categories:
+  - sveltekit
+  - svelte
+published: true
+---
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,18 @@
+import type { Post } from "$lib/types/server";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({cookies}) => {
+export const load: PageServerLoad = async ({cookies, fetch}) => {
+    const promise = fetch('api/posts');
+
     const extendInformation = cookies.get('extendInfo');
     const extendProjects = cookies.get('extendProjects');
     const extendBlog = cookies.get('extendBlog');
 
+    const response = await promise;
+    const posts: Post[] = await response.json();
+
     return {
+        posts: posts,
         cookies: {
             extendInformation: extendInformation === 'true',
             extendProjects: extendProjects === 'true',
