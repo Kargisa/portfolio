50a2fd56bb65975ac8e8e9bbf3e9c5dc5adbb7b2
kargisa <luca.wolf@outlook.com>
2024-03-08 16:35:46 +0100

    bug fixes and recursion

--- src/components/explorer/folder.svelte
+++ src/components/explorer/folder.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
 	import { writeCookie } from '$lib/helpers/cookieHelper';
-	import { ChevronDown, ChevronRight } from 'lucide-svelte';
-	import { onMount } from 'svelte';
+	import { toTitleCase } from '$lib/helpers/stringHelper';
+	import type { Category, Post } from '$lib/types/server';
+	import { ChevronDown, ChevronRight, Orbit } from 'lucide-svelte';
+	import { createEventDispatcher } from 'svelte';
+	import File from './file.svelte';
 
 	export let extend: boolean;
 	export let name: string;
 	export let cookie: string;
+	export let recursiveCategories: Category[] = [];
+	export let recursivePosts: Post[] = [];
+	/** defines the selected file for sub files and folders and if this folder is selected */
+	export let selectedFile: string = 'null';
+
+	const dispatcher = createEventDispatcher();
 </script>
 
 <div class="flex flex-col justify-center space-y-1">
@@ -24,8 +33,32 @@
 		<p>{name}</p>
 	</button>
 	{#if extend}
-		<div class="flex flex-col space-y-1 pl-6">
+		<div class="flex flex-col space-y-1 pl-4">
 			<slot />
+			{#each recursivePosts as post}
+				<File
+					href={`?post=${post.slug}`}
+					label={post.slug}
+					name={post.title}
+					bind:selectedFile
+					on:dblclick={() => {
+						dispatcher('filedbclick', { label: post.slug, name: post.title });
+					}}
+				>
+					<Orbit class="size-5" />
+				</File>
+			{/each}
+			{#each recursiveCategories as category}
+				<svelte:self
+					extend={false}
+					name={toTitleCase(category.title)}
+					cookie={`extend${toTitleCase(category.title)}`}
+					recursiveCategories={category.categories}
+					recursivePosts={category.posts}
+					bind:selectedFile
+					on:filedbclick={(event) => dispatcher('filedbclick', { ...event.detail })}
+				/>
+			{/each}
 		</div>
 	{/if}
 </div>
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,21 +3,11 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({url, cookies, fetch}) => {
     const promis = fetch('api/posts');
-
-    const extendInformation = cookies.get('extendInfo');
-    const extendProjects = cookies.get('extendProjects');
-    const extendBlog = cookies.get('extendBlog');
-
     
     const response = await promis;
     const categories: Category[] = await response.json();
     
     return {
         categories: categories,
-        cookies: {
-            extendInformation: extendInformation === 'true',
-            extendProjects: extendProjects === 'true',
-            extendBlog: extendBlog === 'true'
-        }
     }
 }
